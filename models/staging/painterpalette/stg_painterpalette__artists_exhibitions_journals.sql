d7d295cbeb44dec80c731f3a4e99da2671c0ed23
Clara Luna Lizana Calvo <clarzana+painterpalette@gmail.com>
2025-11-26 13:54:31 +0000

    staging

--- models/staging/painterpalette/stg_painterpalette__artists_exhibitions_journals.sql
+++ models/staging/painterpalette/stg_painterpalette__artists_exhibitions_journals.sql
@@ -1,17 +1,18 @@
 with
-source as (
+source_ej as (
     select *
     from
         {{ ref('base_painterpalette__exhibitions_journals') }}
 ),
 renamed as (
-
     select
+        {{ dbt_utils.generate_surrogate_key([dbt_utils.generate_surrogate_key(['ej.id', 'ej.title']),
+        dbt_utils.generate_surrogate_key(['trim(split_artist.value, \'"\')'])]) }}::varchar(32) as artist_exhibition_journal_id,
 
-        {{ dbt_utils.generate_surrogate_key(['source.id', 'source.title']) }}::varchar(32) as exhibition_journal_id,
-        {{ dbt_utils.generate_surrogate_key(['trim(split_artist.value, \'"\')']) }}::varchar(32) as artist_name
-        from painterpalette_dev_bronze_db.bronze_raw.exhibitions_journals ej, lateral flatten(input => ej.artists) as split_artist
-    from source
+        {{ dbt_utils.generate_surrogate_key(['ej.id', 'ej.title']) }}::varchar(32) as exhibition_journal_id,
+        {{ dbt_utils.generate_surrogate_key(['trim(split_artist.value, \'"\')']) }}::varchar(32) as artist_id,
+        contains(ej.title_artists, trim(split_artist.value, '"'))::boolean as in_title
+    from source_ej ej, lateral flatten(input => ej.artists) as split_artist
 )
 
 select * from renamed
